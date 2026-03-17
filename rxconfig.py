92e9bea190c0a3fbb0b3a66e44baaaff2ac6865e
Adam <adamsmithkirthik@gmail.com>
2026-03-17 13:21:41 +0530

    Explicitly enable SitemapPlugin in rxconfig to satisfy Reflex warning

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,8 @@
 import reflex as rx
+from reflex.plugins.sitemap import SitemapPlugin
 
 config = rx.Config(
     app_name="mySite",
     db_url="sqlite:///resume.db",
+    plugins=[SitemapPlugin()],
 )
